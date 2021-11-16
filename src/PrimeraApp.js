0bd7d39c8aff4a093455918e15a6a977a8666e30
Davidcastel26 <davcastellanosalrios@gmail.com>
2021-11-16 13:34:50 -0600

    importing fragment into the component

---
+++ src/PrimeraApp.js
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+
+const PrimeraApp = () =>{
+    return(
+        <Fragment>
+            <h1>Hola mundo</h1>
+            <p>Mi primera App</p>
+
+        </Fragment>
+    )
+}
+
+export default PrimeraApp;
