01703eff94747f1f1889aa60867593e51cdac6d8
Davidcastel26 <davcastellanosalrios@gmail.com>
2021-11-16 14:17:18 -0600

    adding requirement for props

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,9 +1,17 @@
 import { Fragment } from "react";
+//to request some props as a must use proptype 
+import PropTypes from 'prop-types';
 
-const PrimeraApp = () =>{
+const PrimeraApp = ( {wave} ) =>{
+    // -------------- to request some props as a must ------------------------
+    // if(!wave){
+    //     throw new Error('the wave is has been required')
+    // }
+    // const saludo = 'Hola Mundo'
+    // ----------------------------------------------------------------------
     return(
         <Fragment>
-            <h1>Hola mundo</h1>
+            <h1> {wave} </h1>
             <p>Mi primera App</p>
 
         </Fragment>
