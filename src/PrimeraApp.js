57e8826d0d44e8d84478654187b088149ea6f05c
Davidcastel26 <davcastellanosalrios@gmail.com>
2021-11-17 10:50:50 -0600

    changin component

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,8 +1,9 @@
 import { Fragment } from "react";
 //to request some props as a must use proptype 
 import PropTypes from 'prop-types';
+import CounterApp from "./CounterApp";
 
-const PrimeraApp = ( {wave} ) =>{
+const PrimeraApp = ( {wave, subTitle="I wish you the best!"} ) =>{
     // -------------- to request some props as a must ------------------------
     // if(!wave){
     //     throw new Error('the wave is has been required')
@@ -12,10 +13,15 @@ const PrimeraApp = ( {wave} ) =>{
     return(
         <Fragment>
             <h1> {wave} </h1>
-            <p>Mi primera App</p>
-
+            <p> {subTitle} </p>
+            <CounterApp value={23}  />
         </Fragment>
     )
+}
+
+PrimeraApp.propTypes = {
+    wave: PropTypes.string.isRequired,
+    subTitle: PropTypes.string.isRequired
 }
 
 export default PrimeraApp;
